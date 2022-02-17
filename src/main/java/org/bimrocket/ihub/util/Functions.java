e7eb0a15ce6a1fe0cc32b4ff11c7a2c411f4c85d
wilberquito <wbq.software@gmail.com>
2022-02-17 09:49:15 +0100

    Function to delete local file

--- src/main/java/org/bimrocket/ihub/util/Functions.java
+++ src/main/java/org/bimrocket/ihub/util/Functions.java
@@ -1,8 +1,13 @@
 package org.bimrocket.ihub.util;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -10,6 +15,8 @@ import java.util.Map;
  */
 public class Functions
 {
+    private static final Logger log = LoggerFactory.getLogger(Functions.class);
+
     /**
      * Builds a map using two list the first one represent map keys and the
      * second represents map values
@@ -60,4 +67,24 @@ public class Functions
         }
         return nuls == dict.size();
     }
+
+    /**
+     * Search for a file alocated in `path` if it is a file tries to delete it
+     * 
+     * @param path
+     */
+    public static void deleteFile(String path)
+    {
+        try
+        {
+            Files.deleteIfExists(Paths.get(path));
+        }
+        catch (Exception e)
+        {
+            log.error(
+                    "deleteFile@InventoryUtils: could not delete file - {} - correclty -  exception",
+                    path, e.getMessage());
+        }
+    }
+
 }
