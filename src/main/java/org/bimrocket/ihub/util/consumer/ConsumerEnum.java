a012542b5b1699de14bb5807874bc4e8b8095485
wilberquito <wbq.software@gmail.com>
2022-02-25 13:18:29 +0100

    Adds author

--- src/main/java/org/bimrocket/ihub/util/consumer/ConsumerEnum.java
+++ src/main/java/org/bimrocket/ihub/util/consumer/ConsumerEnum.java
@@ -1,5 +1,9 @@
 package org.bimrocket.ihub.util.consumer;
 
+/**
+ * 
+ * @author wilberquito
+ */
 public enum ConsumerEnum
 {
     MOBA, EXCEL_HTTP, EXCEL_FTP
--- src/main/java/org/bimrocket/ihub/util/consumer/IConsumer.java
+++ src/main/java/org/bimrocket/ihub/util/consumer/IConsumer.java
@@ -2,6 +2,10 @@ package org.bimrocket.ihub.util.consumer;
 
 import java.io.InputStream;
 
+/**
+ * 
+ * @author wilberquito
+ */
 public interface IConsumer
 {
     public InputStream consum() throws Exception;
