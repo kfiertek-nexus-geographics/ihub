da7468875ddacb7757837704aeb0b8c5af5ca006
NG\kfiertek <kfiertek@nexusgeographics.com>
2022-03-08 22:01:43 +0100

    fixes

--- src/main/java/org/bimrocket/ihub/InventoryHubApp.java
+++ src/main/java/org/bimrocket/ihub/InventoryHubApp.java
@@ -30,15 +30,8 @@
  */
 package org.bimrocket.ihub;
 
-import java.util.Arrays;
-
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.ServletContext;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -54,17 +47,4 @@ public class InventoryHubApp
     SpringApplication.run(InventoryHubApp.class, args);
   }
   
-  
-  @Bean
-  ServletRegistrationBean jsfServletRegistration (ServletContext servletContext) {
-      //spring boot only works if this is set
-      servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
-
-      //FacesServlet registration
-      ServletRegistrationBean srb = new ServletRegistrationBean();
-      srb.setServlet(new FacesServlet());
-      srb.setUrlMappings(Arrays.asList("*.xhtml"));
-      srb.setLoadOnStartup(1);
-      return srb;
-  }
 }
